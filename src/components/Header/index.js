5f71f00b161563ac321c84da48fe53057c380b2a
Seungjin <infinissible@gmail.com>
2022-08-02 22:48:48 -0700

    media query added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,7 @@ import Nav from '../Navigation';
 
 function Header() {
   return (
-    <header className="shadow-bottom flex-row space-between pb-30 p-50">
+    <header className="shadow-bottom flex-row">
       <Nav />
     </header>
   );
